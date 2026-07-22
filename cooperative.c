7159b49b7833560550706563d3319af039163489
PrinceLemayian <189862853+PrinceLemayian@users.noreply.github.com>
2026-07-22 12:46:22 +0300

    add farmer records

--- cooperative.c
+++ cooperative.c
@@ -1,1 +1,24 @@
-#include<stdio.h>
+#include <stdio.h>
+#include <string.h>
+
+#define SIZE 6
+
+int main(void) {
+  int farmerNumbers[SIZE] = {101, 102, 103, 104, 105, 106};
+
+  char farmerNames[SIZE][50] = {"Mwangi Kamau",   "Wanjiku Njeri",
+                                "Peter Maina",    "Akinyi Otieno",
+                                "Mutiso Musyoka", "Chebet Kiptoo"};
+
+  char produceTypes[SIZE][20] = {"Milk",  "Coffee", "Potatoes",
+                                 "Maize", "Milk",   "Vegetables"};
+
+  int quantities[SIZE] = {120, 85, 300, 250, 150, 95};
+
+  double pricesPerUnit[SIZE] = {55, 120, 40, 65, 55, 80};
+
+  char paymentStatus[SIZE][10] = {"Pending", "Paid", "Pending",
+                                  "Pending", "Paid", "Pending"};
+
+  return 0;
+}
